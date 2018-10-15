80f813a5fc126243c47d08d8542bd5883cf8fa55
DevExpressExampleBot <fakeEmail>
2018-10-15 12:02:19 +0300

    Source auto update [skip ci]

--- VB/Expander/Form1.vb
+++ VB/Expander/Form1.vb
@@ -29,12 +29,12 @@ Namespace Expander
 
         Private Sub SetAutoCorrectOptions()
 '            #Region "#options"
-            Dim correctionOptions As AutoCorrectOptions = richEditControl1.Options.AutoCorrect
+            Dim сorrectionOptions As AutoCorrectOptions = richEditControl1.Options.AutoCorrect
 
-            correctionOptions.CorrectTwoInitialCapitals = True
-            correctionOptions.DetectUrls = False
-            correctionOptions.ReplaceTextAsYouType = True
-            correctionOptions.UseSpellCheckerSuggestions = True
+            сorrectionOptions.CorrectTwoInitialCapitals = True
+            сorrectionOptions.DetectUrls = False
+            сorrectionOptions.ReplaceTextAsYouType = True
+            сorrectionOptions.UseSpellCheckerSuggestions = True
 '            #End Region ' #options
 
         End Sub
